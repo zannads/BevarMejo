049296841f9916bb379272f780f1d38a17f129fb
Dennis <65568528+zannads@users.noreply.github.com>
2023-07-04 23:37:15 +0200

    Moved link to EPANET library at the beginning
    In this way is always available, pros and cons....+
    Also renamed *logger* to *bemelib_logger*

--- lib/logger/populationsHandler.cpp
+++ lib/logger/populationsHandler.cpp
@@ -7,9 +7,7 @@
 
 #include "populationsHandler.hpp"
 
-namespace beme {
-
-using namespace std;
+namespace bevarmejo {
 
 template<>
 void writeParamCouple<std::vector<double>> (std::ofstream& ofs, const char* paramName, std::vector<double> paramValue){
@@ -23,20 +21,20 @@ void writeParamCouple<std::vector<double>> (std::ofstream& ofs, const char* para
     ofs <<sJunk <<std::endl;
 }
 
-void writeHeader(ofstream& ofs, pagmo::population& pop){
+void writeHeader(std::ofstream& ofs, pagmo::population& pop){
     
-    string sJunk{};
+    std::string sJunk{};
     
     sJunk ="Current time: ";
     const auto now = std::chrono::system_clock::now();
     const time_t t_c = std::chrono::system_clock::to_time_t(now);
     sJunk += ctime(&t_c);
-    ofs <<sJunk <<endl;
+    ofs <<sJunk <<std::endl;
     
     //writeParamCouple(ofs, "Problem name: ", pop.get_problem().get_name());
     sJunk = "Problem name: ";
     sJunk += pop.get_problem().get_name();
-    ofs << sJunk <<endl;
+    ofs << sJunk <<std::endl;
     
     writeParamCouple(ofs, "\tGlobal dimension: ", pop.get_problem().get_nx());
     
@@ -51,7 +49,7 @@ void writeHeader(ofstream& ofs, pagmo::population& pop){
     writeParamCouple(ofs, "\tFitness evaluations: ", pop.get_problem().get_fevals());
 }
 
-void saveFinalPopulation(filesystem::path& outFilename, pagmo::population& pop){
+void saveFinalPopulation(std::filesystem::path& outFilename, pagmo::population& pop){
 
     /*
     if (!exists(outFilename) || !is_regular_file(outFilename)){
@@ -65,26 +63,26 @@ void saveFinalPopulation(filesystem::path& outFilename, pagmo::population& pop){
         outFilename = default_outFilename;
     }*/
     
-    ofstream ofs;
+    std::ofstream ofs;
     ofs.open(outFilename);
     if(!ofs.is_open()){
-        cout <<"Problem with outfile" <<endl;
-        cout << pop;
+        std::cout <<"Problem with outfile" <<std::endl;
+        std::cout << pop;
     }
     
     writeHeader(ofs, pop);
 
     // Write body
-    ofs <<endl;
+    ofs <<std::endl;
     writeParamCouple(ofs, "Population size: ", pop.size());
     
-    ofs <<endl << "List of individuals: " <<endl;
+    ofs <<std::endl << "List of individuals: " <<std::endl;
     auto ids = pop.get_ID();
     auto dvs = pop.get_x();
     auto fis = pop.get_f();
-    string sJunk{};
+    std::string sJunk{};
     for( unsigned int i = 0; i<pop.size(); ++i){
-        sJunk = "#"; sJunk += to_string(i); sJunk += ":\n";
+        sJunk = "#"; sJunk += std::to_string(i); sJunk += ":\n";
         ofs << sJunk;
         
         writeParamCouple(ofs, "\tID:\t", ids.at(i));
