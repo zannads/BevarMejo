859b6f332728e4dda5cd837fd98de837bee764c0
Dennis <65568528+zannads@users.noreply.github.com>
2023-07-25 11:47:09 +0200

    I O generic stream

--- bevarmejolib/include/bevarmejo/io.hpp
+++ bevarmejolib/include/bevarmejo/io.hpp
@@ -9,6 +9,7 @@
 #define BEVARMEJOLIB_IO_HPP
 
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <string>
 #include <string_view>
@@ -17,192 +18,217 @@
 
 namespace bevarmejo {
 
+namespace detail {
+
+/* BASIC TYPES stream
+* For basic types, if the type we pass doesn't have a >> operator
+* a compile-error will appear
+* CAREFUL: with strings when parsing as input only the first word
+* before the space is parsed in, but not in out...
+*/
+
 template <typename T>
-struct NMatrix {
-    std::vector<std::size_t> dimensions;
-    std::vector<T> data;
-
-    NMatrix<T>& load(std::istream& is, const std::string_view tag) {
-        dimensions.clear();
-        data.clear();
-
-        // I look in the file until I find the tag
-        std::string line;
-        while (std::getline(is, line))
-        {
-            if (line.find(tag) != std::string::npos) {
-                // Once I find it, data are in the following structure:
-                // #tag n1 n2 n3 ... nN
-                // data11, data12, data13, ... data1n2
-                // data21, data22, data23, ... data2n2
-                // ...
-                // datan11, datan12, datan13, ... datan1n2
-                // ---new_element--- (in dimension n3)
-                // data again
-                // ___NEW_DIMENSION___ (finished loading third dimension)
-                // data again
-
-                std::stringstream ss(line);
-
-                // consume the name #tag
-                std::string name;
-                ss >> name;
-                // I know dimensions follow immediately after the name in the same row,
-                // thus, continue reading from the same stringstream
-                this->dimensions = load_dimensions(ss);
-
-                // Data are from the next line onwards,
-                // thus, continue reading from is again
-                this->data = load_data(is, this->dimensions);
-
-                // TODO: Check if data are consistent with dimensions 
-                // If the number of elements is less than the expected, 
-                // the remaining elements are set to 0 (or whatever the default value is)
-                // If the number of elements is more than the expected, the ones after are ignored 
-            }
-        }
-        is.clear();
-        is.seekg(0, std::ios::beg);
-
-        return *this;
-    }
-    std::size_t ndims() const { return this->dimensions.size(); }
-
-    std::size_t size() const {
-        std::size_t size = 1;
-        for (auto& dim : this->dimensions) {
-            size *= dim;
-        }
-        return size;
-    }
-
-    std::vector<std::size_t> get_dimensions() const { return this->dimensions; }
-
-    std::vector<std::size_t> load_dimensions(std::istream& is) {
-        std::vector<std::size_t> dimensions;
-
-        std::string line;
-        std::getline(is, line);
-
-        // case 1: no numbers after the name
-        if (line.empty()) {
-            dimensions.push_back(1);
-            dimensions.push_back(1);
-            return dimensions;
-        }
-        // case 2: special simbol - after the name
-        if (line.find("-") != std::string::npos) {
-            return dimensions;
-        }
-        // case 3: numbers after the name
-        std::stringstream ss(line);
-        std::size_t n;
-        while (ss >> n) {
-            dimensions.push_back(n);
-        }
-        dimensions.push_back(1);
-        return dimensions;
-    }
-
-    std::vector<T> get_1D_data() const { return this->data; }
-
-    std::vector<T> load_data(std::istream& is, const std::vector<std::size_t>& dimensions) {
-
-        // case 2: special simbol - after the name
-        if (dimensions.size() == 0) {
-            return std::vector<T>();
-        }
-
-        // case 1, 3: numbers after the name (implicit or explicit)
-        std::size_t n_required = this->size();
-        std::vector<T> data(n_required);
-        std::size_t n_loaded = 0;
-        std::vector<std::size_t> indices(this->ndims() - 1, 0); // indices of the current element
-        // I never need the last dimension as it is always 1
-        std::string line;
-
-        while (std::getline(is, line) && n_loaded < n_required) {
-            // Alwys remove \t in case someone added them
-            // (if parsed into string object they will appear as \t)
-            line.erase(std::remove(line.begin(), line.end(), '\t'), line.end());
-            // Here I check if there are the special separators for the dimensions
-            // (---new_element--- and ___NEW_DIMENSION___)
-            // TODO: check separators and correct the position of the indices
-
-            // For now only the first and second dimension are supported
-            // special case for 1D matrices
-            if (indices.size() > 1)
-                indices[1] = 0; // reset the second dimension as each line contains all the elements of that dimension
-
-            /* From here on I always work with bidimensional matrices */
-            // put the current row back in a stream to parse it
-            std::stringstream ss(line);
-            T value;
-            // If second dimension is 1, or I am at the last element of the second
-            // dimension just load with normal getline, i.e. separator is \n
-            // Otherwise, I need to use the comma separator and save them in the
-            // correct loaction
-            char separator = (indices.size() == 1) || (indices[1] == (dimensions[1] - 1)) ? '\n' : ',';
-
-            while (std::getline(ss, line, separator)) {
-                //TODO: here if type is string it takes only the first word
-                std::stringstream ss2(line);
-                ss2 >> value;
-                data[this->NIndices_to_index(indices)] = value;
-                if (indices.size() > 1)
-                    ++indices[1];   // Next column of this bidimensional slice
-                ++n_loaded;     // I loaded one more element
-            }
-
-            ++indices[0];       // Next row of this bidimensional slice
-        }
-
-        return data;
-    }
-
-    std::size_t NIndices_to_index(const std::vector<std::size_t>& indices) const {
-        // TODO: check that indices are a vector smaller than dimensions by one
-
-        std::size_t index = 0;
-        std::size_t stride = 1;
-        for (std::size_t i = 0; i < indices.size(); ++i) {
-            index += indices[i] * stride;
-            stride *= this->dimensions[i];
-        }
-        return index;
-    }
-};
-
-
-std::stringstream get_data_from_tag(std::istream& is, const std::string_view tag);
-
-
-template <typename T, typename U>
-inline void stream_param(std::ostream &os, const T& param_name, const U& param_value){
-    os << param_name <<param_value <<std::endl;
+inline void stream_input(std::istream& is, T& value) {
+    is >> value;
 }
 
-template <typename T, typename U>
-inline void stream_param(std::ostream &os, const T& vector_name, const std::vector<U>& vector_value ){
+template <typename T>
+inline void stream_output(std::ostream& os, const T& value) {
+    os << value;
+}
+
+/* TYPE SPECIFIC implementations */
+
+/* TODO: bool */
+
+/* TODO: pair */
+
+/* CONTAINERS */
+
+// Helpers for single elements containers: array, vector, list, set, etc..
+template<typename It>
+inline void stream_input_single_range(std::istream& is, It begin, const It end,
+                                      const std::string_view opener = " ",
+                                       const std::string_view delimeter = " ",
+                                       const std::string_view closer = " ") {
     
-    os << vector_name;
-    
-    os << "[";
-    
-    auto end= vector_value.end();
-    for(auto i= vector_value.begin(); i != end; ){
-        os << *i;
-        if (++i != end ){
-            os << ", ";
+    // Move to the opener
+    is.ignore(1000, opener[0]);
+
+    // Now I am in the best position to start parsing. 
+    // I will parse until the stream ends
+    while(begin!= end) {
+        // Parse the first element
+        stream_input(is, *begin);
+        
+        // Unless it is the last look for the delimeter, otherwise make sure you will exit and then look for the closer
+        if (++begin != end){
+            // Move to the delimeter
+            is.ignore(1000, delimeter[0]);
         }
     }
     
-    os << "]\n";
+    // Move to the closer
+    is.ignore(1000, closer[0]);
+}
+
+template<typename It>
+inline void stream_output_single_range(std::ostream& os, It begin, const It end,
+                                      const std::string_view opener = " ",
+                                       const std::string_view delimeter = " ",
+                                       const std::string_view closer = " ") {
     
+    os << opener;
     
+    while (begin != end) {
+        stream_output(os, *begin);
+        
+        // Unless it is the last put the delimeter,
+        // Note that if it is empty you never enter the while in first place
+        if (++begin != end){
+            os << delimeter;
+        }
+    }
+    
+    os << closer;
     return;
 }
 
-}/* namespace bevarmejo */
+// Helpers for couple elements containers: map, etc ...
+template<typename It>
+inline void stream_input_couple_range(std::istream& is, It begin, const It end,
+                                      const std::string_view opener = " ",
+                                       const std::string_view delimeter = " ",
+                                       const std::string_view closer = " ",
+                                      const std::string_view el_delimeter = " ") {
+    
+    // Move to the opener
+    is.ignore(1000, opener[0]);
+
+    // Now I am in the best position to start parsing. 
+    // I will parse until the stream ends
+    while(begin!= end) {
+        // Parse the first element
+        stream_input(is, begin->first);
+        
+        // Move to the delimeter
+        is.ignore(1000, el_delimeter[0]);
+        
+        // Parse the second element
+        stream_input(is, begin->second);
+        
+        // Unless it is the last look for the delimeter, otherwise make sure you will exit and then look for the closer
+        if (++begin != end){
+            // Move to the delimeter
+            is.ignore(1000, delimeter[0]);
+        }
+    }
+
+    // Move to the closer
+    is.ignore(1000, closer[0]);
+}
+
+template<typename It>
+inline void stream_output_couple_range(std::ostream& os, It begin, const It end,
+                                       const std::string_view opener = " ",
+                                      const std::string_view delimeter = " ",
+                                      const std::string_view closer = " ",
+                                       const std::string_view el_delimeter = " ") {
+    
+    os << opener;
+    
+    while (begin != end) {
+        stream_output(os, begin->first);
+        stream_output(os, el_delimeter);
+        stream_output(os, begin->second);
+        
+        // Unless it is the last put the delimeter,
+        // Note that if it is empty you never enter the while in first place 
+        if (++begin != end){
+            os << delimeter;
+        }
+    }
+    
+    os << closer;
+    return;
+}
+
+/* VECTOR
+* Structure of vectors:
+* [1, 2, 3, 5]
+*/
+template <typename U>
+inline void stream_input(std::istream& is, std::vector<U> &v) {
+    stream_input_single_range(is, v.begin(), v.end(), "[", ",", "]" );
+}
+
+template <class U>
+inline void stream_output(std::ostream &os, const std::vector<U> &v) {
+    stream_output_single_range(os, v.begin(), v.end(), "[", ", ", "]" );
+}
+
+/* MAP
+* Structure of maps:
+* {1| 2
+*  2 | 3
+*  3 | 5} 
+*/
+template <class Key, class U>
+inline void stream_output(std::ostream &os, const std::map<Key,U> &m) {
+    stream_output_couple_range(os, m.begin(), m.end(), "{", "\n", "}", " | ");
+}
+
+/* STREAM IN and OUT
+* Simply write or load the same element with the appropriate structure.
+* Wrapper for type specific implementations.
+* Possible output:
+* [1, 3, 5]
+*/
+
+template <typename T, typename ...Args>
+inline void stream_input(std::istream& is, T& value, Args& ...args){
+    stream_input(is, value);
+    stream_input(is, args...);
+}
+
+template <typename T, typename ...Args>
+inline void stream_output(std::ostream& os, const T& value, Args& ...args){
+    stream_output(os, value);
+    stream_output(os, args...);
+}
+
+} /* namespace detail */
+
+
+/* STREAM IN and OUT
+* Simply write or load the same element with the appropriate structure.
+* Wrapper of implementation function in detail namespace.
+* Possible output:
+* [1, 3, 5]
+*/
+
+template <typename... Args>
+inline void stream_in(std::istream &is, Args &...args) {
+    detail::stream_input(is, args...);
+}
+
+template <typename... Args>
+inline void stream_out(std::ostream &os, const Args &...args) {
+    detail::stream_output(os, args...);
+}
+
+/* STREAM PARAM
+* Special type of stream out for couples parameter:
+* Possible output:
+* Seed : 3 \n
+*
+*/
+ 
+template <typename T>
+inline void stream_param(std::ostream &os, const std::string& param_name, const T& param_value){
+    detail::stream_output(os, param_name, " : ", param_value, "\n");
+}
+
+} /* namespace bevarmejo */
 
 #endif /* BEVARMEJOLIB_IO_HPP */
