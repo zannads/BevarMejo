0d1aa7044186c0a3c99cc621dc6197df98d3d80a
Dennis <dennis.zanutto@polimi.it>
2024-10-12 14:43:48 +0200

    Prepared test for metadata

--- tests/library_metadata_tests.cpp
+++ tests/library_metadata_tests.cpp
@@ -4,160 +4,151 @@
 
 namespace __bevarmejo::detail {
 
-// Assuming version_year, version_month, and version_release are globally defined constants.
-// Replace them with appropriate values for testing.
-constexpr unsigned int version_year = bevarmejo::detail::version_year;
-constexpr unsigned int version_month = bevarmejo::detail::version_month;
-constexpr unsigned int version_release = bevarmejo::detail::version_release;
+using VM = bevarmejo::VersionManager;
 
-// Test default constructor
-TEST(VersionTest, DefaultConstructor) {
-    bevarmejo::detail::Version default_version;
-    EXPECT_EQ(default_version, bevarmejo::detail::Version(version_year, version_month, version_release));
+class VersionManagerTest : public ::testing::Test 
+{
+protected:
+    unsigned int v_year{0ul};
+    unsigned int v_month{0ul};
+    unsigned int v_release{0ul};
+
+    void SetUp() override 
+    {
+        // Make sure, user and library version are created.
+        VM::user();
+        VM::library();
+
+        // Set the version year, month and release.
+        v_year = VM::library().version().year();
+        v_month = VM::library().version().month();
+        v_release = VM::library().version().release();
+    }
+}; // class VersionManagerTest
+
+TEST_F(VersionManagerTest, DefaultConstructors) 
+{
+    // By default, the user and library version should be the same, and if I 
+    // request the default version, it should be the same as the library version.
+    EXPECT_EQ(VM::v(), VM::library().version());
+    EXPECT_EQ(VM::v(), VM::user().version());
 }
 
-// Test parameterized constructor with valid inputs
-TEST(VersionTest, ParameterizedConstructorValid) {
-    bevarmejo::detail::Version v1(2023, 6, 0);
-    EXPECT_EQ(v1.str(), "v23.06.0");
+class VersionManagerConstructorParametrizedTest : 
+    public VersionManagerTest, public ::testing::WithParamInterface<std::tuple<bool, std::initializer_list<unsigned int>, unsigned int, std::string>>
+{ 
+    void SetUp() override 
+    {
+        VersionManagerTest::SetUp();
+    }
+}; // class VersionManagerConstructorParametrizedTest
 
-    bevarmejo::detail::Version v2(2024, 10, 1);
-    EXPECT_EQ(v2.str(), "v24.10.1");
-
-    bevarmejo::detail::Version v3(2024, 6, 50);
-    EXPECT_EQ(v3.str(), "v24.06.50");
-}
-
-// Test parameterized constructor with invalid dates (year, month, release)
-// A date is invalid if:
-// 1. it is out of project versions range (before 2023.06.0 or after the current version)
-// 2. the parameters are naturally out of range (1 <= month <= 12, 0 <= release <= 99)
-TEST(VersionTest, ParameterizedConstructorInvalidDates) {
-    EXPECT_THROW(bevarmejo::detail::Version(2022, 6, 0), std::invalid_argument);
-    EXPECT_THROW(bevarmejo::detail::Version(version_year+1, 6, 0), std::invalid_argument);
-
-    EXPECT_THROW(bevarmejo::detail::Version(2023, 5, 0), std::invalid_argument);
-    EXPECT_THROW(bevarmejo::detail::Version(2023, 0, 0), std::invalid_argument);
-    EXPECT_THROW(bevarmejo::detail::Version(2023, 13, 0), std::invalid_argument);
-    EXPECT_THROW(bevarmejo::detail::Version(version_year, version_month+1, 0), std::invalid_argument);
-
-    EXPECT_THROW(bevarmejo::detail::Version(2023, 6, 100), std::invalid_argument);
-    EXPECT_THROW(bevarmejo::detail::Version(version_year, version_month, version_release+1), std::invalid_argument);
-}
-
-// Test comparison operators
-TEST(VersionTest, ComparisonOperators) {
-    bevarmejo::detail::Version v1(2023, 6, 0);
-    bevarmejo::detail::Version v2(2023, 6, 1);
-    bevarmejo::detail::Version v3(2023, 7, 0);
-    bevarmejo::detail::Version v4(2024, 6, 0);
-
-    EXPECT_TRUE(v1 < v2);
-    EXPECT_TRUE(v2 < v3);
-    EXPECT_TRUE(v3 < v4);
-    EXPECT_TRUE(v1 < v3);
-    EXPECT_TRUE(v1 < v4);
-    EXPECT_TRUE(v2 < v4);
-
-    EXPECT_TRUE(v4 > v3);
-    EXPECT_TRUE(v4 > v2);
-    EXPECT_TRUE(v4 > v1);
-    EXPECT_TRUE(v3 > v2);
-    EXPECT_TRUE(v3 > v1);
-    EXPECT_TRUE(v2 > v1);
-
-    EXPECT_TRUE(v1 <= v2);
-    EXPECT_TRUE(v1 <= v3);
-    EXPECT_TRUE(v1 <= v4);
-    EXPECT_TRUE(v2 <= v3);
-    EXPECT_TRUE(v2 <= v4);
-    EXPECT_TRUE(v3 <= v4);
+TEST_P(VersionManagerConstructorParametrizedTest, VersionParametrizedConstructors)
+{
+    // Try to create contrusctors with invalid values. 
+    // Let's test initializer list, string and numeric.
+    const auto& [should_succeed, il, n, s] = GetParam();
     
-    EXPECT_TRUE(v4 >= v3);
-    EXPECT_TRUE(v4 >= v2);
-    EXPECT_TRUE(v4 >= v1);
-    EXPECT_TRUE(v3 >= v2);
-    EXPECT_TRUE(v3 >= v1);
-    EXPECT_TRUE(v2 >= v1);
-    
-    EXPECT_TRUE(v1 != v2);
-    EXPECT_TRUE(v1 != v3);
-    EXPECT_TRUE(v1 != v4);
-    EXPECT_TRUE(v2 != v3);
-    EXPECT_TRUE(v2 != v4);
-    EXPECT_TRUE(v3 != v4);
+    if (should_succeed)
+    {
+        ASSERT_NO_THROW(VM::v(il));
+        ASSERT_EQ(VM::v(il).year(), il.begin()[0]);
+        ASSERT_EQ(VM::v(il).month(), il.begin()[1]);
+        ASSERT_EQ(VM::v(il).release(), il.begin()[2]);
 
-    EXPECT_FALSE(v1 == v2);
-    EXPECT_FALSE(v1 == v3);
-    EXPECT_FALSE(v1 == v4);
-    EXPECT_FALSE(v2 == v3);
-    EXPECT_FALSE(v2 == v4);
-    EXPECT_FALSE(v3 == v4);
+        EXPECT_NO_THROW(VM::v(n));
+        EXPECT_EQ(VM::v(n).year(), il.begin()[0]);
+        EXPECT_EQ(VM::v(n).month(), il.begin()[1]);
+        EXPECT_EQ(VM::v(n).release(), il.begin()[2]);
 
-    EXPECT_EQ(v1, v1);
-    EXPECT_EQ(v2, v2);
-    EXPECT_EQ(v3, v3);
-    EXPECT_EQ(v4, v4);
+        EXPECT_NO_THROW(VM::v(s));
+        EXPECT_EQ(VM::v(s).year(), il.begin()[0]);
+        EXPECT_EQ(VM::v(s).month(), il.begin()[1]);
+        EXPECT_EQ(VM::v(s).release(), il.begin()[2]);
+    }
+    else
+    {
+        EXPECT_THROW(VM::v(il), std::invalid_argument);
+        EXPECT_THROW(VM::v(n), std::invalid_argument);
+        EXPECT_THROW(VM::v(s), std::invalid_argument);
+    }
 }
 
-// Test parsing from string
-TEST(VersionTest, ParseFromString) {
-    bevarmejo::detail::Version v = bevarmejo::detail::Version::parse("v23.06.0");
-    EXPECT_EQ(v, bevarmejo::detail::Version(2023, 6, 0));
-    EXPECT_EQ(v.str(), "v23.06.0");
+// Invalid version have:
+//  - a year before 2000 or after the current year.
+//  - an invalid month, i.e., not between 1 and 12, or if the year is the current year, not between 1 and the current month.
+//  - an invalid release, i.e., not between 0 and 99, or if the year and month are the current year and month, not between 0 and the current release.
+// When parsed from string, they must be in the format "vYY.m.r", 
+// where YY is the year, m is the month and r is the release.
+INSTANTIATE_TEST_SUITE_P(
+    VersionManagerConstructorParametrizedTestCases,
+    VersionManagerConstructorParametrizedTest,
+    ::testing::Values(
+        // Valid versions.
+        std::make_tuple(true, {2023, 6, 0}, 230600, std::string("v23.06.0")),
+        std::make_tuple(true, {2023, 6, 30}, 230630, std::string("v23.06.30")),
+        std::make_tuple(true, {2023, 10, 4}, 231004, std::string("v23.10.04")),
+        std::make_tuple(true, {2024, 1, 12}, 240112, std::string("v24.01.12")),
 
-    v = bevarmejo::detail::Version::parse("v24.1.50");
-    EXPECT_EQ(v, bevarmejo::detail::Version(2024, 1, 50));
-    EXPECT_EQ(v.str(), "v24.01.50");
+        // Invalid year.
+        std::make_tuple(false, {1999, 6, 0}, 990600, std::string("v99.06.0")),
+        std::make_tuple(false, {v_year+1, v_month, v_release}, VM::library().version().numeric()+10000, 
+                    std::string("v")+std::to_string(v_year+1)+"."+std::to_string(v_month)+"."+std::to_string(v_release)),
 
-    v = bevarmejo::detail::Version::parse("v24.10.1");
-    EXPECT_EQ(v, bevarmejo::detail::Version(2024, 10, 1));
-    EXPECT_EQ(v.str(), "v24.10.1");
+        // Invalid month.
+        std::make_tuple(false, {2023, 0, 0}, 230000, std::string("v23.00.0")),
+        std::make_tuple(false, {2023, 13, 0}, 231300, std::string("v23.13.0")),
+        std::make_tuple(false, {v_year, v_month+1, v_release}, VM::library().version().numeric()+100, 
+                    std::string("v")+std::to_string(v_year)+"."+std::to_string(v_month+1)+"."+std::to_string(v_release)),
+
+        // Invalid release.
+        std::make_tuple(false, {2023, 6, 100}, 231600, std::string("v23.06.100")),
+        std::make_tuple(false, {v_year, v_month, v_release+1}, VM::library().version().numeric()+1, 
+                    std::string("v")+std::to_string(v_year)+"."+std::to_string(v_month)+"."+std::to_string(v_release+1))
+    )
+);
+
+// Test the conversion to numeric and string.
+class VMAccessParTest : 
+    public VersionManagerTest, public ::testing::WithParamInterface<std::tuple<std::initializer_list<unsigned int>, unsigned int, std::string>>
+{ 
+    void SetUp() override 
+    {
+        VersionManagerTest::SetUp();
+    }
+}; // class VMAccessParTest
+TEST_P(VMAccessParTest, VersionAccess)
+{
+    const auto& [il, n, s] = GetParam();
+    const auto v = VM::v(il);
+
+    EXPECT_EQ(v.numeric(), n);
+    EXPECT_EQ(v.str(), s);
+}
+INSTANTIATE_TEST_SUITE_P(
+    VMAccessParTestCases,
+    VMAccessParTest,
+    ::testing::Values(
+        std::make_tuple(std::initializer_list<unsigned int>{2023, 6, 0}, 230600, std::string("v23.06.0")),
+        std::make_tuple(std::initializer_list<unsigned int>{2023, 6, 30}, 230630, std::string("v23.06.30")),
+        std::make_tuple(std::initializer_list<unsigned int>{2023, 10, 4}, 231004, std::string("v23.10.04")),
+        std::make_tuple(std::initializer_list<unsigned int>{2024, 1, 12}, 240112, std::string("v24.01.12")),
+        std::make_tuple(std::initializer_list<unsigned int>{2023, 10, 45}, 231045, std::string("v23.10.45")), 
+    )
+);
+
+// Test the comparison operators.
+// I have to try the 27 combination of year less, equal and greater, then month and release.
+// test by version, numeric and string.
+TEST_F(VersionManagerTest, VersionComparison)
+{
+    const auto v = VM::v({2023, 6, 50});
 }
 
-// Test parsing from invalid string 
-// Invalid strings are those that do not follow the format "vYY.MM.r"
-TEST(VersionTest, ParseFromStringInvalid) {
-    EXPECT_THROW(bevarmejo::detail::Version::parse("23.06.0"), std::invalid_argument);
-    EXPECT_THROW(bevarmejo::detail::Version::parse("v23.06"), std::invalid_argument);
+// Test parse of the numeric 
 
-    EXPECT_NO_THROW(bevarmejo::detail::Version::parse("v23.06.0.1"));
-    EXPECT_NO_THROW(bevarmejo::detail::Version::parse("v23.6.0"));
-    EXPECT_NO_THROW(bevarmejo::detail::Version::parse("blablav23.06.0"));
-}
+// Test parse of the string
 
-// Test comparison with string versions
-TEST(VersionTest, ComparisonWithString) {
-    bevarmejo::detail::Version v(2023, 6, 0);
-
-    EXPECT_TRUE(v < "v24.10.1");
-    EXPECT_TRUE(v <= "v24.10.1");
-    EXPECT_TRUE(v != "v24.10.1");
-    EXPECT_FALSE(v == "v24.10.1");
-    EXPECT_TRUE(v == "v23.06.0");
-
-    EXPECT_TRUE(v > "v23.05.0");
-    EXPECT_TRUE(v >= "v23.05.0");
-    EXPECT_FALSE(v < "v23.05.0");
-    EXPECT_FALSE(v <= "v23.05.0");
-}
-
-// Test static numeric function
-TEST(VersionTest, StaticNumericFunction) {
-    unsigned int numeric_value = bevarmejo::detail::Version::numeric(2023, 6, 0);
-    EXPECT_EQ(numeric_value, 230600);
-
-    numeric_value = bevarmejo::detail::Version::numeric(2024, 10, 1);
-    EXPECT_EQ(numeric_value, 241001);
-}
-
-// Test static string conversion function
-TEST(VersionTest, StaticStringFunction) {
-    std::string version_str = bevarmejo::detail::Version::str(2023, 6, 0);
-    EXPECT_EQ(version_str, "v23.06.0");
-
-    version_str = bevarmejo::detail::Version::str(2024, 10, 1);
-    EXPECT_EQ(version_str, "v24.10.1");
-}
+// Test the set of the user version.
 
 } // namespace __bevarmejo::detail
