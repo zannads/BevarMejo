e77048b7b7ca234fa7daf41c41a2f474fd35389d
Dennis <dennis.zanutto@polimi.it>
2024-11-15 14:43:35 +0100

    RegistryRange access methods (relaying on TBD iterators)
    sim[ ]
    opt[ ]

--- bevarmejolib/include/bevarmejo/utility/registry_range.hpp
+++ bevarmejolib/include/bevarmejo/utility/registry_range.hpp
@@ -104,7 +104,108 @@ public:
 public:
     
 /*------- Element access -------*/
+public:
+    mapped_type& at(const key_type& id)
+    {
+        return const_cast<mapped_type&>(std::as_const(*this).at(id));
+    }
+    const mapped_type& at(const key_type& id) const
+    {
+        if (!m__registry)
+            __format_and_throw<std::runtime_error>("RegistryRange", "at", "The registry is not set.");
 
+        // If the style is Exclude, asking for an element that is in the list of excluded elements, it is like asking for a non-existing element.
+        // If the style is Include or OrderedInclude, asking for an element that is not in the list of included elements, it is like asking for a non-existing element.
+        if constexpr (std::is_same_v<style_type, Exclude>)
+        {
+            if (inherited::contains(id))
+                __format_and_throw<std::out_of_range>("RegistryRange", "at", "The element is excluded.");
+        }
+        else // if constexpr (std::is_same_v<style_type, Include> || std::is_same_v<style_type, OrderedInclude>)
+        {
+            if (!inherited::contains(id))
+                __format_and_throw<std::out_of_range>("RegistryRange", "at", "The element is not included.");
+        }
+        
+        return m__registry->at(id);
+    }
+    reference at(size_type pos) { return *(begin()+pos); }
+    const_reference at(size_type pos) const { return *(cbegin()+pos); }
+
+    reference front() { return *begin(); }
+    const_reference front() const { return *begin(); }
+
+    reference back() { return *(end() - 1); }
+    const_reference back() const { return *(end() - 1); }
+
+/*--- Iterators ---*/
+public:
+    iterator begin() noexcept { return iterator(this, 0); }
+    const_iterator begin() const noexcept { return const_iterator(this, 0); }
+    const_iterator cbegin() const noexcept { return const_iterator(this, 0); }
+
+    iterator end() noexcept { return iterator(this, m__registry->size()); }
+    const_iterator end() const noexcept { return const_iterator(this, m__registry->size()); }
+    const_iterator cend() const noexcept { return const_iterator(this, m__registry->size()); }
+
+    reverse_iterator rbegin() noexcept { return reverse_iterator(this, m__registry->size()); }
+    const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(this ,m__registry->size()); }
+    const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(this, m__registry->size()); }
+
+    reverse_iterator rend() noexcept { return reverse_iterator(this, 0); }
+    const_reverse_iterator rend() const noexcept { return const_reverse_iterator(this, 0); }
+    const_reverse_iterator crend() const noexcept { return const_reverse_iterator(this, 0); }
+
+/*--- Capacity ---*/
+public:
+    // Can not mark the methods noexcept because they use the find method that
+    // can throw (because of string comparison).
+    bool empty() const { return size() > 0; }
+
+    size_type size() const 
+    {
+        if (m__registry == nullptr)
+            return 0;
+
+        if constexpr (std::is_same_v<Style, Exclude>)
+        {
+            size_type count = m__registry->size();
+
+            // Remove the elements to exclude but only if they actually exist
+            for (const auto& id : m__elements)
+            {
+                if (m__registry->contains(id))
+                    --count;
+            }
+
+            return count;
+        }
+
+        // if constrexpr (std::is_same_v<Style, Include> || std::is_same_v<Style, OrderedInclude>)
+        size_type count = 0;
+
+        // Count the elements to include but only if they actually exist
+        for (const auto& id : m__elements)
+        {
+            if (m__registry->contains(id))
+                ++count;
+        }
+
+        return count;
+    }
+
+// These are not needed for the RegistryRange. They are already implemented in the UniqueStringSequence.
+    // size_type max_size() const noexcept;
+
+    // void reserve(size_type new_cap);
+
+    // size_type capacity() const;
+
+    // void shrink_to_fit();
+
+/*--- Modifiers ---*/
+public:
+    // Modifiers are not needed for the RegistryRange. They are already implemented in the UniqueStringSequence.
     
 }; // class RegistryRange
 
