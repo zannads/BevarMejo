5051e15ad0f9ee27fd5014179acde2aee7e16bb1
Dennis <dennis.zanutto@polimi.it>
2024-11-18 16:06:35 +0100

    A first version of a forward iterator
    sim[ ]
    opt[ ]

--- bevarmejolib/include/bevarmejo/utility/registry_range.hpp
+++ bevarmejolib/include/bevarmejo/utility/registry_range.hpp
@@ -51,26 +51,21 @@ private:
     // Forward declaration of the iterator.
     template <class RV>
     class Iterator;
-    template <class RV>
-    class ReverseIterator;
 public:
     using iterator = Iterator<self_type>;
     using const_iterator = Iterator<const self_type>;
-    using reverse_iterator = ReverseIterator<self_type>;
-    using const_reverse_iterator = ReverseIterator<const self_type>;
 
 private:
     friend iterator;
-    friend const_iterator;
 
 /*------- Member objects -------*/
 private:
 #ifdef ENABLE_SAFETY_CHECKS
-    SafeMemberPtr<Reg> mp__registry;
-    std::weak_ptr<USS> mp__elements;
+    SafeMemberPtr<Reg> mp__registry; // My Pointer to Registry
+    std::weak_ptr<USS> mp__u_ids; // My Pointer to Unique ID Sequence
 #else
-    Reg* mp__registry;
-    USS* mp__elements;
+    Reg* mp__registry; // My Pointer to Registry
+    USS* mp__u_ids; // My Pointer to Unique ID Sequence
 #endif
 
 /*------- Member functions -------*/
@@ -79,15 +74,15 @@ public:
     RegistryView() noexcept = default;
     RegistryView(SafeMemberPtr<Reg> registry) noexcept : 
         mp__registry(registry), 
-        mp__elements(nullptr)
+        mp__u_ids(nullptr)
     { }
     RegistryView(std::weak_ptr<USS> elements) noexcept : 
         mp__registry(nullptr), 
-        mp__elements(elements)
+        mp__u_ids(elements)
     { }
     RegistryView(SafeMemberPtr<Reg> registry, std::weak_ptr<USS> elements) noexcept : 
         mp__registry(registry), 
-        mp__elements(elements)
+        mp__u_ids(elements)
     { }
     RegistryView(const RegistryView &other) = default;
     RegistryView(RegistryView &&other) noexcept = default;
@@ -112,7 +107,7 @@ public:
     }
     const mapped_type& at(const key_type& id) const
     {
-        if (!mp__registry)
+        if (!valid(mp__registry))
             __format_and_throw<std::out_of_range>("RegistryView", "at", "Element not found.",
                 "The registry is not available.");
 
@@ -120,13 +115,13 @@ public:
         // If the style is Include or OrderedInclude, asking for an element that is not in the list of included elements, it is like asking for a non-existing element.
         if constexpr (std::is_same_v<behaviour_type, ViewBehaviour::Exclude>)
         {
-            if (mp__elements!= nullptr && mp__elements->contains(id))
+            if (valid(mp__u_ids) && mp__u_ids->contains(id))
                 __format_and_throw<std::out_of_range>("RegistryView", "at", "Element not found.",
                     "The element is excluded.");
         }
         else // if constexpr (std::is_same_v<behaviour_type, ViewBehaviour::Include> || std::is_same_v<behaviour_type, ViewBehaviour::OrderedInclude>)
         {
-            if (mp__elements!= nullptr && !mp__elements->contains(id))
+            if (valid(mp__u_ids) && !mp__u_ids->contains(id))
                 __format_and_throw<std::out_of_range>("RegistryView", "at", "The element is not included.",
                     "The element is not in the list of included elements.");
         }
@@ -144,21 +139,13 @@ public:
 
 /*--- Iterators ---*/
 public:
-    iterator begin() noexcept { return iterator(this, 0); }
-    const_iterator begin() const noexcept { return const_iterator(this, 0); }
-    const_iterator cbegin() const noexcept { return const_iterator(this, 0); }
+    iterator begin() noexcept { return iterator(this); }
+    const_iterator begin()  const noexcept { return const_iterator(this); }
+    const_iterator cbegin() const noexcept { return const_iterator(this); }
 
-    iterator end() noexcept { return iterator(this, mp__registry->size()); }
-    const_iterator end() const noexcept { return const_iterator(this, mp__registry->size()); }
-    const_iterator cend() const noexcept { return const_iterator(this, mp__registry->size()); }
-
-    reverse_iterator rbegin() noexcept { return reverse_iterator(this, mp__registry->size()); }
-    const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(this ,mp__registry->size()); }
-    const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(this, mp__registry->size()); }
-
-    reverse_iterator rend() noexcept { return reverse_iterator(this, 0); }
-    const_reverse_iterator rend() const noexcept { return const_reverse_iterator(this, 0); }
-    const_reverse_iterator crend() const noexcept { return const_reverse_iterator(this, 0); }
+    iterator end() noexcept { return iterator(this, /*end = */ true); }
+    const_iterator end()  const noexcept { return const_iterator(this, /*end = */ true); }
+    const_iterator cend() const noexcept { return const_iterator(this, /*end = */ true); }
 
 /*--- Capacity ---*/
 public:
@@ -168,7 +155,7 @@ public:
 
     size_type size() const 
     {
-        if (mp__registry == nullptr)
+        if (!valid(mp__registry))
             return 0;
 
         if constexpr (std::is_same_v<B, ViewBehaviour::Exclude>)
@@ -176,7 +163,7 @@ public:
             size_type count = mp__registry->size();
 
             // Remove the elements to exclude but only if they actually exist
-            for (const auto& id : value_or_empty(mp__elements))
+            for (const auto& id : value_or_empty(mp__u_ids))
             {
                 if (mp__registry->contains(id))
                     --count;
@@ -190,7 +177,7 @@ public:
             size_type count = 0;
 
             // Count the elements to include but only if they actually exist
-            for (const auto& id : value_or_empty(mp__elements))
+            for (const auto& id : value_or_empty(mp__u_ids))
             {
                 if (mp__registry->contains(id))
                     ++count;
@@ -220,7 +207,7 @@ public:
             typename RV::Reg::const_iterator,
             typename RV::Reg::iterator
         >::type;
-        using iterator_category = std::bidirectional_iterator_tag;
+        using iterator_category = std::forward_iterator_tag;
         using value_type = typename base_iter::value_type;
         using difference_type = typename base_iter::difference_type;
         using pointer = typename base_iter::pointer;
@@ -234,29 +221,72 @@ public:
         RV* m__range;
 #endif
         base_iter m__iter;
-        UniqueStringSequence& m__ids;
+        bool f__end;
 
     /*--- Member functions ---*/
     /*--- (constructor) ---*/
     public:
-        Iterator() = delete;
-        Iterator(RV* range, size_type index) noexcept :
+        Iterator() noexcept :
+            m__range(nullptr), 
+            m__iter(),
+            f__end(true)
+        { }
+        // For begin operator
+        Iterator(RV* range) noexcept :
+            Iterator(range, false)
+        { }
+        // For end operator
+        Iterator(RV* range, bool end) noexcept :
             m__range(range), 
-            m__iter(range->mp__registry->begin()),
-            m__ids(*range)
+            m__iter(),
+            f__end()
         {
-            // Based on the B, I need to find valid element in the Registry. 
-            // It could easily go all the way to the end iterator.
-            // Use try catch in case ENABLE_SAFETY_CHECKS is on.
+            // If end iterator, nothing to be done. Otherwise, I need to find the first valid element.
+            // It's an end iterator when: I ask, or ur view doesn't point to a registry, or the registry is empty.
+            if (end || !valid(m__range) || !valid(m__range->mp__registry) || m__range->mp__registry->empty())
+            {
+                f__end = true;
+                return;
+            }
+            
+            // Based on the B, I need to find the first valid element in the Registry. 
+            // It could easily go all the way to the end iterator again.
+            // Use try catch to guarantee noexcept in case ENABLE_SAFETY_CHECKS is on.
             try
             {
-                m__iter += index;
+                if constexpr (std::is_same_v<typename RV::behaviour_type, ViewBehaviour::Exclude>)
+                {
+                    m__iter = m__range->mp__registry->begin();
+                    while (m__iter != m__range->mp__registry->end() && m__range->mp__u_ids->contains(m__iter->id))
+                        ++m__iter;
+                }
+
+                if constexpr (std::is_same_v<typename RV::behaviour_type, ViewBehaviour::Include>)
+                {
+                    m__iter = m__range->mp__registry->begin();
+                    while (m__iter != m__range->mp__registry->end() && !m__range->mp__u_ids->contains(m__iter->id))
+                        ++m__iter;
+                }
+
+                if constexpr (std::is_same_v<typename RV::behaviour_type, ViewBehaviour::OrderedInclude>)
+                {
+                    auto curr_id = m__range->mp__u_ids->begin();
+                    do
+                    {
+                        m__iter = m__range->mp__registry->find(*curr_id);
+                        ++curr_id;
+                    }
+                    while (m__iter == m__range->mp__registry->end() && curr_id != m__range->mp__u_ids->end());
+                }
+
+                f__end = m__iter == m__range->mp__registry->end();
+
             }
-            catch (...)
+            catch(const std::exception& e)
             {
-                m__iter = range->mp__registry->end();
+                std::cerr << e.what() << '\n';
             }
-        }
+        }            
         Iterator(const Iterator &other) noexcept = default;
         Iterator(Iterator &&other) noexcept = default;
 
@@ -271,62 +301,193 @@ public:
 
     /*--- base ---*/
     protected:
-        base_iter base() const { return m__iter; }
+        base_iter& base() { return m__iter; }
+        const base_iter& base() const { return m__iter; }
 
     /*--- access operators ---*/
     public:
-        reference operator*() const { return *m__iter; }
+        reference operator*() const {
+#ifdef ENABLE_SAFETY_CHECKS
+            if (f__end)
+                __format_and_throw<std::out_of_range>("RegistryView::Iterator::operator*()", "Impossible to dereference the iterator.",
+                    "The iterator is at the end.");
+#endif
+            return *m__iter;
+        }
 
-        pointer operator->() const { return m__iter.operator->(); }
+        pointer operator->() const { 
+#ifdef ENABLE_SAFETY_CHECKS
+            if (f__end)
+                __format_and_throw<std::out_of_range>("RegistryView::Iterator::operator->()", "Impossible to dereference the iterator.",
+                    "The iterator is at the end.");
+#endif
+            return m__iter.operator->();
+        }
 
     /*--- increment/decrement operators ---*/
     public:
         iterator_type &operator++()
         {
+#ifdef ENABLE_SAFETY_CHECKS
+            if (f__end)
+                __format_and_throw<std::out_of_range>("RegistryView::Iterator::operator++()", "Impossible to increment the iterator.",
+                    "The iterator is at the end.");
+            if (!valid(m__range) || !valid(m__range->mp__registry))
+                __format_and_throw<std::out_of_range>("RegistryView::Iterator::operator++()", "Impossible to increment the iterator.",
+                    "The registry is not available.");
+            if (m__iter == m__range->mp__registry->end())
+                __format_and_throw<std::out_of_range>("RegistryView::Iterator::operator++()", "Impossible to increment the iterator.",
+                    "The iterator is at the end.");
+#endif
+            // Based on the style, I need to find the next valid element in the Registry.
+            // If it is exclude, I move forward more than once, if my next element is in the list of excluded elements.
+            // If it is include, I move forward more than once, if my next element is not in the list of included elements.
+            // If it is ordered include, I simply find the next one
+
+            if constexpr (std::is_same_v<typename RV::behaviour_type, ViewBehaviour::Exclude>)
+            {
+                do
+                {
+                    ++m__iter;
+                } while (m__iter != m__range->mp__registry->end() && m__range->mp__u_ids->contains(m__iter->id));
+            }
+            
+            if constexpr (std::is_same_v<typename RV::behaviour_type, ViewBehaviour::Include>)
+            {
+                do
+                {
+                    ++m__iter;
+                } while (m__iter != mp__registry->end() && !m__range->mp__u_ids->contains(m__iter->id));
+            }
+            
+            if constexpr (std::is_same_v<typename RV::behaviour_type, ViewBehaviour::OrderedInclude>)
+            {
+                // I am pointing at ID x, find it in the USS, get the next, find it in the registry, get the iterator.
+                auto curr_id = m__range->mp__u_ids->find(m__iter->id);
+                auto next_id = std::next(curr_id);
+
+                // If I can't find it (should not happen at all) or I am at the end of the list, I am done.
+                if (curr_id == m__ids.end() || next_id == m__ids.end())
+                {
+                    m__iter = m__range->mp__registry->end();
+                    f__end = true;
+                    return *this;
+                }
+                
+                // else, I have a valid next element, find it in the registry.
+                m__iter = m__range->mp__registry->find(*next_id);
+            }
+
+            f__end = m__iter == m__range->mp__registry->end();  
 
             return *this;
         }
         iterator_type operator++(int) {auto tmp= *this; ++(*this); return tmp;}
 
-        iterator_type &operator--()
-        {
-
-            return *this;
-        }
-        iterator_type operator--(int) {auto tmp= *this; --(*this); return tmp;}
-
-        iterator_type &operator+=(difference_type n)
+        iterator_type &operator+=(size_type n)
         {
             while (n)
-            {
-                if (n > 0)
-                {
-                    ++(*this);
+                ++(*this);
                     --n;
-                }
-                else
-                {
-                    --(*this);
-                    ++n;
-                }
-            }
 
             return *this;
         }
-        iterator_type operator+(difference_type n) const {auto tmp= *this; return tmp += n;}
-        iterator_type &operator-=(difference_type n) {return (*this += (-n));}
-        iterator_type operator-(difference_type n) const {return (*this + (-n));}
+        iterator_type operator+(size_type n) const {auto tmp= *this; return tmp += n;}
+
         difference_type operator-(const iterator_type &other) const { return m__iter - other.m__iter; }
 
     /*--- comparison operators ---*/
     public:
-        bool operator==(const iterator_type &other) const { return m__iter == other.m__iter; }
-        bool operator!=(const iterator_type &other) const { return m__iter != other.m__iter; }
-        bool operator<(const iterator_type &other) const { return m__iter < other.m__iter; }
-        bool operator>(const iterator_type &other) const { return m__iter > other.m__iter; }
-        bool operator<=(const iterator_type &other) const { return m__iter <= other.m__iter; }
-        bool operator>=(const iterator_type &other) const { return m__iter >= other.m__iter; }
+        bool operator==(const iterator_type &other) const
+        {
+#ifdef ENABLE_SAFETY_CHECKS
+            check_comparable(other);
+#endif
+            if (f__end && other.f__end)
+                return true;
 
+            return f__end == other.f__end && m__iter == other.m__iter;
+        }
+
+        bool operator!=(const iterator_type &other) const
+        {
+#ifdef ENABLE_SAFETY_CHECKS
+            check_comparable(other);
+#endif
+            if (f__end && other.f__end)
+                return false;
+            
+            return f__end != other.f__end || m__iter != other.m__iter;
+        }
+
+        bool operator<(const iterator_type &other) const
+        {
+#ifdef ENABLE_SAFETY_CHECKS
+            check_comparable(other);
+#endif
+            if (f__end && other.f__end)
+                return false;
+
+            if (other.f__end)
+                return true;
+
+            return !f__end && m__iter < other.m__iter;
+        }
+
+        bool operator>(const iterator_type &other) const
+        {
+#ifdef ENABLE_SAFETY_CHECKS
+            check_comparable(other);
+#endif
+            if (f__end && other.f__end)
+                return false;
+
+            if (f__end)
+                return true;
+
+            return !other.f__end && m__iter > other.m__iter;
+        }
+
+        bool operator<=(const iterator_type &other) const
+        {
+#ifdef ENABLE_SAFETY_CHECKS
+            check_comparable(other);
+#endif    
+            if (f__end && other.f__end)
+                return true;
+
+            if (other.f__end)
+                    return false;
+
+            return !f__end && m__iter <= other.m__iter;
+        }
+
+        bool operator>=(const iterator_type &other) const {
+#ifdef ENABLE_SAFETY_CHECKS
+            check_comparable(other);
+#endif
+            if (f__end && other.f__end)
+                return true;
+
+            if (f__end)
+                return false;
+
+            return !other.f__end && m__iter >= other.m__iter;
+        }
+
+// Helper for check if the iterator is valid.
+#ifdef ENABLE_SAFETY_CHECKS
+    private:    
+        void check_comparable(const iterator_type &other) const
+        {
+            if (!valid(m__range) || !valid(m__range->mp__registry))
+                __format_and_throw<std::out_of_range>("RegistryView::Iterator", "operator==", "Impossible to compare the iterators.",
+                    "The registry is not available.");
+            if (m__range != other.m__range)
+                __format_and_throw<std::out_of_range>("RegistryView::Iterator", "operator==", "Impossible to compare the iterators.",
+                    "The iterators are not from the same registry.");
+        }
+#endif    
     }; // class RegistryView::Iterator
 
 }; // class RegistryView
