4db9fbe10f8766b3e694a6a80980bcbe51c6680f
Dennis <dennis.zanutto@polimi.it>
2025-01-02 15:26:28 +0100

    FIX type_traits for test
    Test[V]

--- bevarmejolib/include/bevarmejo/utility/type_traits.hpp
+++ bevarmejolib/include/bevarmejo/utility/type_traits.hpp
@@ -1,8 +1,10 @@
-#ifndef BEVARMEJOLIB__TYPE_TRAITS_HPP
-#define BEVARMEJOLIB__TYPE_TRAITS_HPP
+#ifndef BEVARMEJOLIB__UTILITY__TYPE_TRAITS_HPP
+#define BEVARMEJOLIB__UTILITY__TYPE_TRAITS_HPP
 
+#include <memory>
 #include <tuple>
 #include <type_traits>
+#include <typeinfo>
 #include <vector>
 
 namespace bevarmejo
@@ -31,6 +33,10 @@ struct contains_type;
 template <typename T, typename... Us>
 struct contains_type<T, std::tuple<Us...>> : std::disjunction<std::is_same<T, Us>...> {};
 
+// Convenience alias for contains_type
+template <typename T, typename Tuple>
+inline constexpr bool contains_type_v = contains_type<T, Tuple>::value;
+
 // Helper to get the index of a type in a tuple
 template <typename T, typename Tuple>
 struct index_of_type;
@@ -51,11 +57,15 @@ struct index_of_type<T, std::tuple<Us...>>
     template <std::size_t Index>
     struct index_of_type_impl<Index>
     {
-        static_assert(std::true_type::value, "Type not found in the tuple.");
+        static constexpr std::size_t value = Index;
     };
 
     static constexpr std::size_t value = index_of_type_impl<0, Us...>::value;
 };
+
+// Convenience alias for index_of_type
+template <typename T, typename Tuple>
+inline constexpr std::size_t index_of_type_v = index_of_type<T, Tuple>::value;
 
 namespace detail
 {
@@ -63,15 +73,51 @@ namespace detail
 template <typename Tuple>
 class EnumClassEqCore
 {
+/*----------------------------------------------------------------------------*/
+/*---------------------------- Member types ----------------------------------*/
+/*----------------------------------------------------------------------------*/
+protected:
+    using self_type = EnumClassEqCore<Tuple>;
+    using types = Tuple;
+
+/*----------------------------------------------------------------------------*/
+/*---------------------------- Member objects --------------------------------*/
+/*----------------------------------------------------------------------------*/
+private:
+
+/*----------------------------------------------------------------------------*/
+/*--------------------------- Member functions -------------------------------*/
+/*----------------------------------------------------------------------------*/
+// (constructor)
+public:
+    EnumClassEqCore() = default;
+    EnumClassEqCore(const EnumClassEqCore&) = default;
+    EnumClassEqCore(EnumClassEqCore&&) = default;
+
+// (destructor)
 public:
     virtual ~EnumClassEqCore() = default;
 
-// Templated "is" method
+// operator=
+public:
+    EnumClassEqCore& operator=(const EnumClassEqCore&) = default;
+    EnumClassEqCore& operator=(EnumClassEqCore&&) = default;
+
+// clone
+public:
+    std::unique_ptr<self_type> clone() const 
+    {
+        return std::unique_ptr<self_type>(this->__clone());
+    }
+private:
+    virtual self_type* __clone() const = 0;
+
+// Templated "is" method to check the type
 public:
     template <typename T>
     bool is() const
     {
-        static_assert(bevarmejo::contains_type<T, Tuple>, "Type is not allowed because it is not in the tuple.");
+        static_assert(bevarmejo::contains_type_v<T, types>, "This type is not a valid type for this selector.");
         return __is(typeid(T));
     }
 
@@ -86,17 +132,61 @@ protected:
 
 }; // class EnumClassEqCore
 
-} // namespace detail
-
 template <typename Tuple, typename Tag>
 class EnumClassEq final : public detail::EnumClassEqCore<Tuple>
 {
+/*----------------------------------------------------------------------------*/
+/*---------------------------- Member types ----------------------------------*/
+/*----------------------------------------------------------------------------*/
+private:
+    using self_type = EnumClassEq<Tuple, Tag>;
+    using base_type = detail::EnumClassEqCore<Tuple>;
+    using types = Tuple;
+
+/*----------------------------------------------------------------------------*/
+/*---------------------------- Member objects --------------------------------*/
+/*----------------------------------------------------------------------------*/
+private:
+
+/*----------------------------------------------------------------------------*/
+/*--------------------------- Member functions -------------------------------*/
+/*----------------------------------------------------------------------------*/
+// (constructor)
+public:
+    EnumClassEq()
+    {
+        static_assert(bevarmejo::contains_type_v<Tag, types>, "Tag is not allowed because it is not in the tuple.");
+    }
+    EnumClassEq(const EnumClassEq&) = default;
+    EnumClassEq(EnumClassEq&&) = default;
+
+// (destructor)
+public:
+    ~EnumClassEq() override = default;
+
+// operator=
+public:
+    EnumClassEq& operator=(const EnumClassEq&) = default;
+    EnumClassEq& operator=(EnumClassEq&&) = default;
+
+// clone
+public:
+    // No need to override the base clone method 
+
+private:
+    self_type* __clone() const override
+    {
+        return new self_type(*this);
+    }
+
+// Hidden virtual method for the "is" method
 protected:
     bool __is(const std::type_info& type) const override
     {
         return typeid(Tag) == type;
     }
 
+// Pure virtual method to get the value
 public:
     std::size_t value() const override
     {
@@ -105,15 +195,17 @@ public:
 
 }; // class EnumClassEq
 
+} // namespace detail
+
 template <typename Tuple>
 using TagsSelector = std::unique_ptr<detail::EnumClassEqCore<Tuple>>;
 
 template <typename Tuple, typename Tag>
 TagsSelector<Tuple> make_selector()
 {
-    return std::make_unique<EnumClassEq<Tuple, Tag>>();
+    return std::make_unique<detail::EnumClassEq<Tuple, Tag>>();
 }
 
 } // namespace bevarmejo
 
-#endif // BEVARMEJOLIB__TYPE_TRAITS_HPP
+#endif // BEVARMEJOLIB__UTILITY__TYPE_TRAITS_HPP
