4e795339e5c4c5060c96a2e42cfd766c1f4863c2
Dennis <dennis.zanutto@polimi.it>
2025-02-24 21:20:50 +0100

    AliasedKeys are now declared constexpr
    sim[V]
    opt[V]

--- bevarmejolib/include/bevarmejo/io/keys/beme.hpp
+++ bevarmejolib/include/bevarmejo/io/keys/beme.hpp
@@ -2,19 +2,20 @@
 
 #include "bevarmejo/io/aliased_key.hpp"
 
-namespace bevarmejo::io::key {
+namespace bevarmejo::io::key
+{
 
-static const AliasedKey beme_version{"Bemelib version"}; // "Bemelib version"
+static constexpr bevarmejo::io::AliasedKey beme_version{"Bemelib version"}; // "Bemelib version"
 
-static const AliasedKey problem{"Problem", "UDP"}; // "Problem", "UDP"
+static constexpr bevarmejo::io::AliasedKey problem{"Problem", "UDP"}; // "Problem", "UDP"
 #if BEME_VERSION < 240601
-static const AliasedKey type{"Type", "Name"}; // "Type", "Name" (backward compatibility)
+static constexpr bevarmejo::io::AliasedKey type{"Type", "Name"}; // "Type", "Name" (backward compatibility)
 #else
-static const AliasedKey type{ "Type" }; // "Type"
+static constexpr bevarmejo::io::AliasedKey type{ "Type" }; // "Type"
 #endif
-static const AliasedKey name{"Name"}; // "Name"
-static const AliasedKey params{"Parameters", "Params"}; // "Parameters"
+static constexpr bevarmejo::io::AliasedKey name{"Name"}; // "Name"
+static constexpr bevarmejo::io::AliasedKey params{"Parameters", "Params"}; // "Parameters"
 
-static const AliasedKey lookup_paths{"Lookup paths", "Paths"}; // "Lookup paths", "Paths"
+static constexpr bevarmejo::io::AliasedKey lookup_paths{"Lookup paths", "Paths"}; // "Lookup paths", "Paths"
 
 }   // namespace bevarmejo::io::key
--- bevarmejolib/include/bevarmejo/io/keys/bemeexp.hpp
+++ bevarmejolib/include/bevarmejo/io/keys/bemeexp.hpp
@@ -4,23 +4,23 @@
 
 namespace bevarmejo::io::key {
 
-static const AliasedKey islands{"Islands"}; // "Islands"
-static const AliasedKey errors{"Errors"}; // "Errors"
-static const AliasedKey system{"System"}; // "System"
-static const AliasedKey software{"Software"}; // "Software"
-static const AliasedKey t0{"Time start"}; // "Time start"
-static const AliasedKey tend{"Time end"}; // "Time end"
+static constexpr bevarmejo::io::AliasedKey islands{"Islands"}; // "Islands"
+static constexpr bevarmejo::io::AliasedKey errors{"Errors"}; // "Errors"
+static constexpr bevarmejo::io::AliasedKey system{"System"}; // "System"
+static constexpr bevarmejo::io::AliasedKey software{"Software"}; // "Software"
+static constexpr bevarmejo::io::AliasedKey t0{"Time start"}; // "Time start"
+static constexpr bevarmejo::io::AliasedKey tend{"Time end"}; // "Time end"
 
-static const AliasedKey extras{"Extra info"}; // "Extra info"
+static constexpr bevarmejo::io::AliasedKey extras{"Extra info"}; // "Extra info"
 
-static const AliasedKey ctime{"Current time"}; // "Current time"
-static const AliasedKey fevals{"Fitness evaluations", "Fevals"}; // "Fitness evaluations", "Fevals"
-static const AliasedKey gevals{"Gradient evaluations", "Gevals"}; // "Gradient evaluations", "Gevals"
-static const AliasedKey hevals{"Hessian evaluations", "Hevals"}; // "Hessian evaluations", "Hevals"
-static const AliasedKey individuals{"Individuals"}; // "Individuals"
+static constexpr bevarmejo::io::AliasedKey ctime{"Current time"}; // "Current time"
+static constexpr bevarmejo::io::AliasedKey fevals{"Fitness evaluations", "Fevals"}; // "Fitness evaluations", "Fevals"
+static constexpr bevarmejo::io::AliasedKey gevals{"Gradient evaluations", "Gevals"}; // "Gradient evaluations", "Gevals"
+static constexpr bevarmejo::io::AliasedKey hevals{"Hessian evaluations", "Hevals"}; // "Hessian evaluations", "Hevals"
+static constexpr bevarmejo::io::AliasedKey individuals{"Individuals"}; // "Individuals"
 
-static const AliasedKey id{"ID"}; // "ID"
-static const AliasedKey dv{"Decision vector", "DV"}; // "Decision vector", "DV"
-static const AliasedKey fv{"Fitness vector", "FV"}; // "Fitness vector", "FV"
+static constexpr bevarmejo::io::AliasedKey id{"ID"}; // "ID"
+static constexpr bevarmejo::io::AliasedKey dv{"Decision vector", "DV"}; // "Decision vector", "DV"
+static constexpr bevarmejo::io::AliasedKey fv{"Fitness vector", "FV"}; // "Fitness vector", "FV"
 
 }   // namespace bevarmejo::io::key
