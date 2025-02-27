1aedfa55140253d3a45a4179ec692fd0f46b4a83
Dennis <dennis.zanutto@polimi.it>
2025-02-27 14:52:56 +0100

    Refactored the cli s to work with the same flow
    sim[V]
    opt[V]

--- cli/bemeopt.cpp
+++ cli/bemeopt.cpp
@@ -1,36 +1,43 @@
-// 
-//  main_anytown_test.cpp
-//	anytown original test
-//
-//  Created by Dennis Zanutto on 11/07/23.
-
 #include <iostream>
 
-#include "bevarmejo/cli_settings.hpp"
-#include "bevarmejo/utility/exceptions.hpp"
+#include "bevarmejo/utility/io.hpp"
 #include "bevarmejo/experiment.hpp"
-#include "bevarmejo/parsers.hpp"
 
-int main(int argc, char* argv[]) {   
-    
+int main(int argc, char* argv[])
+{
     // 1. Parse the inputs, ideally I could change anything and should perform checks.
     // argv[1] the settings file (it also implicitly defines the experiment folder unless copy flag is active)
-    bevarmejo::ExperimentSettings settings;
+    bevarmejo::Experiment experiment;
     try {
-        settings = bevarmejo::opt::parse(argc, argv);
+        experiment = bevarmejo::Experiment::parse(argc, argv);
     }
     catch (const std::exception& e) {
         bevarmejo::io::stream_out(std::cerr, "An error happend while parsing the CLI inputs:\n", e.what(), "\n" );
         return 1;
     }
-    bevarmejo::Experiment experiment(settings.settings_file);
+
+    try {
+        experiment.pre_run_tasks();
+    }
+    catch (const std::exception& e) {
+        bevarmejo::io::stream_out(std::cerr, "An error happend while doing the tasks in preparation of the experiment run:\n", e.what(), "\n" );
+        return 2;
+    }
+
     try {
         experiment.run();
     }
     catch (const std::exception& e) {
-        bevarmejo::io::stream_out(std::cerr, "An error happend while building or running the experiment:\n", e.what(), "\n" );
-        // experiment.finalise_with_error();
-        return 1;
+        bevarmejo::io::stream_out(std::cerr, "An error happend while running the experiment:\n", e.what(), "\n" );
+        return 3;
+    }
+
+    try {
+        experiment.post_run_tasks();
+    }
+    catch( const std::exception& e) {
+        bevarmejo::io::stream_out(std::cerr, "An error happend while doing the tasks after the experiment run:\n", e.what(), "\n" );
+        return 4;
     }
     
     return 0;
